820aed14b7aa152bd07c7197109123b71abe5264
Олега <degoleg@yandex.ru>
2023-02-03 21:34:41 +0400

    all soft

--- run.py
+++ run.py
@@ -3,13 +3,14 @@ from flask import Flask
 # Импортируем блюпринт
 from app.main.views import main_blueprint
 
+
 # Создаем экземпляр Flask
 app = Flask(__name__)
 
 # регистрируем первый блюпринт
 app.register_blueprint(main_blueprint)
 
-# Запускаем сервер, только если файл запущен, а не импортирован
 
+# Запускаем сервер, только если файл запущен, а не импортирован
 if __name__ == "__main__":
     app.run()
